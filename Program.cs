6aa33b266d4e032ae7463844599f83a1f8bfa6c2
fejzo9 <fejzo999@gmail.com>
2025-02-14 14:27:32 +0100

    In Program.cs file changed and adjusted. Added code for connecting with DB, code for ASP.NET Core Identity, for Radzen services, for Blazor Server services, configuration fok middleware, and mapping Blazor and API routes.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,46 @@
 ﻿using Radzen;
-using IDEA_Holding_Test.Components;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using IDEA_Holding_Test.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Dodao Radzen servise
+// Dodata konekcija sa bazom podataka
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+// Dodat ASP.NET Core Identity
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+})
+    .AddRoles<IdentityRole>()  // Dodata podrška za uloge
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Dodat Radzen servise
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
 builder.Services.AddScoped<TooltipService>();
 builder.Services.AddScoped<ContextMenuService>();
 
-// Ostale postojeće konfiguracije
+// Dodat Blazor Server servise
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
 
-app.UseAuthentication();
-app.UseAuthorization();
+// Konfiguracija middleware-a
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();  // Omogućena autentifikaciju
+app.UseAuthorization();   // Omogućena autorizaciju
 
+// Mapiranje Blazor i API ruta
 app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToFile("index.html");
